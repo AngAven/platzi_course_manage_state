1f11d7b16db4bcdc75fd07c727d4da83fd569be8
Angel <angel.aven.c@gmail.com>
2022-01-26 01:03:31 -0600

    Protecting effects

--- src/UseState.jsx
+++ src/UseState.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 function UseState({name}){
   const [error, setError] = React.useState(false)
+  const [loading, setLoading] = React.useState(false)
+
+  React.useEffect(() => {
+    console.log('empieza')
+    if (!!loading) {
+      setTimeout(() => {
+        console.log('Iniciando settimeout')
+        setLoading(false)
+        console.log('Terminando settimeout')
+      }, 1000)
+    }
+    console.log('termina')
+  }, [loading])
 
   return (
     <div>
@@ -12,9 +25,13 @@ function UseState({name}){
         <p>Error: Código incorrecto</p>
       )}
 
+      {loading && (
+        <p>Loading ...</p>
+      )}
+
       <input placeholder="Código de seguridad"/>
       <button
-        onClick={() => setError(!error)}
+        onClick={() => setLoading(!loading)}
       >Comprobar
       </button>
     </div>
